361221d8d41640b82620c5055fce73c407d06e8f
Madhu Latha <madhulatha12340@gmail.com>
2024-10-24 15:47:44 +0530

    DAY_1

---
+++ Day1.java
@@ -0,0 +1,26 @@
+class Day1 {
+    public static void main(String[] args) {
+        System.out.println("hi am starting java programing");
+        String Madhu = "JESUS_tq_for_guieding_me";
+
+        int latha = 143;
+        System.out.println(Madhu);
+        System.out.println(latha);
+        if (12 == 2) {
+            System.out.println("hi am if");
+            System.out.println("hi am if");
+
+        } else {
+            System.out.println("hi am else");
+        }
+        System.out.println("hi am helping to annayya");
+        String Animuthyam = "boy";
+        if (Animuthyam.equals("boy")) {
+            System.out.println("right");
+
+        } else {
+            System.out.println("left");
+        }
+
+    }
+}
